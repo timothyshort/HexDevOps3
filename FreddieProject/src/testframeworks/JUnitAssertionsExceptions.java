b06843dbc5c480f8afd4a08bbbe4bcce10800af4
Tim <timothyjames.short@gmail.com>
2018-07-02 21:58:12 -0400

    Added xpath

--- FreddieProject/src/testframeworks/JUnitAssertionsExceptions.java
+++ FreddieProject/src/testframeworks/JUnitAssertionsExceptions.java
@@ -35,6 +35,7 @@ public class JUnitAssertionsExceptions {
 		driver.findElement(By.name("ctl00$MainContent$btnLogin")).click();
 		boolean expected = false;
 		
+		// looking a specific element and extracted the text
 		if (expected) {
 			try {
 				Assert.assertTrue("Login message not as expected", driver.findElement(By.id("conf_message")).getText().contains("Logged in"));
@@ -49,10 +50,11 @@ public class JUnitAssertionsExceptions {
 				fail("Could not find invalid message element");
 			}
 		}
-	
-		// extract the login message
-		
 
+		// look at the entire page
+		String pageText = driver.findElement(By.xpath("html/body")).getText();
+		if (expected) { Assert.assertTrue(pageText.contains("Logged in successfully")); }
+		else { Assert.assertTrue(pageText.contains("Invalid password")); }
 		
 	}
 
